64caa8011938a378ed849c12b7c1b93d2b9134bc
Jose Miguel Garcia <jmgbenayas@gmail.com>
2017-11-20 19:29:39 +0100

    Ejercicio 3-12 corregido.

--- Ruby/Ejercicios/Ejercicio_3_12/Fecha.rb
+++ Ruby/Ejercicios/Ejercicio_3_12/Fecha.rb
@@ -2,8 +2,8 @@
 require 'date'
 class Fecha
     def procesar_fecha(s_fecha)
-        partes = s_fecha.scan(/(\d+)(\d+)(\d+)/)
-        puts partes[0][0]
+        partes = s_fecha.scan(/([0-9]+)[\-\/\ ]([0-9]+)[\-\/\ ]([0-9]+)/)
+        puts "#{partes[0][0]}-#{partes[0][1]}-#{partes[0][2]}"
         ##procesar_fecha = Date.new(partes[0][2].to_i,partes[0][1].to_i,partes[0][0].to_i)
     end
 end
