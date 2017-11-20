152c48a0d6ee77c3e70a55345ac1b895a5fce963
Jose Miguel Garcia <jmgbenayas@gmail.com>
2017-11-20 19:39:50 +0100

    Corregido Ejercicio 3-12

--- Ruby/Ejercicios/Ejercicio_3_12/App.rb
+++ Ruby/Ejercicios/Ejercicio_3_12/App.rb
@@ -1,5 +1,5 @@
 require './Fecha'
 fecha = Fecha.new()
-fecha.procesar_fecha("15-1-1998")
-fecha.procesar_fecha("23 04 2015")
-fecha.procesar_fecha("01/01/1970")
+puts fecha.procesar_fecha("15-1-1998")
+puts fecha.procesar_fecha("23 04 2015")
+puts fecha.procesar_fecha("01/01/1970")
--- Ruby/Ejercicios/Ejercicio_3_12/Fecha.rb
+++ Ruby/Ejercicios/Ejercicio_3_12/Fecha.rb
@@ -3,7 +3,6 @@ require 'date'
 class Fecha
     def procesar_fecha(s_fecha)
         partes = s_fecha.scan(/([0-9]+)[\-\/\ ]([0-9]+)[\-\/\ ]([0-9]+)/)
-        puts "#{partes[0][0]}-#{partes[0][1]}-#{partes[0][2]}"
-        ##procesar_fecha = Date.new(partes[0][2].to_i,partes[0][1].to_i,partes[0][0].to_i)
+        procesar_fecha = Date.new(partes[0][2].to_i,partes[0][1].to_i,partes[0][0].to_i)
     end
 end
